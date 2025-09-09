652d03a35432333978a010682736bcad08bd6bbb
lilwondermelo <lilwondermelo@gmail.com>
2025-09-09 21:29:29 +0700

    1

--- clear_cache.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (function_exists('opcache_reset')) {
-    opcache_reset();
-    echo "OPcache has been cleared!";
-} else {
-    echo "OPcache is not enabled or the function opcache_reset() is not available.";
-}
-?>
